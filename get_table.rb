1f3954b0ee3fd899b9a4b824d1ce3656990cc1bf
Yuki Fujiwara <sky.y.0079@gmail.com>
2017-11-01 02:57:35 +0000

    11/01 授業

--- get_table.rb
+++ get_table.rb
@@ -7,9 +7,9 @@ html = open(url).read
 doc = Nokogiri::HTML(html)
 
 # 下の行をコメントアウトすると、pryが起動する
-# binding.pry
+#binding.pry
 
 # 例：h2要素のみを抜き出す
-doc.css('h2').each do |node|
+doc.css('table').each do |node|
   puts node.text
 end
